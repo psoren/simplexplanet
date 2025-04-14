617a1b1992fdc1bcb064e9fbf0a9fda90eb8f58f
Parker Sorenson <p.sorenson@me.com>
2025-04-13 21:11:22 -0700

    asdf

---
+++ src/planetTypes.js
@@ -0,0 +1,42 @@
+export const planetTypes = {
+    'terra-nova': {
+        name: 'Terra Nova',
+        description: 'Earth-like planet with diverse biomes',
+        terrainSet: ['deepOcean', 'shallowOcean', 'beach', 'plains', 'hills', 'mountains', 'snowPeaks']
+    },
+    'glacius': {
+        name: 'Glacius',
+        description: 'Ice planet with frozen oceans',
+    terrainSet: ['deepIce', 'shallowIce', 'iceBeach', 'tundra', 'iceHills', 'iceMountains', 'icePeaks']
+    },
+    'jungle-prime': {
+        name: 'Jungle Prime',
+        description: 'Tropical planet with dense vegetation',
+        terrainSet: ['deepJungleOcean', 'shallowJungleOcean', 'jungleBeach', 'jungle', 'jungleHills', 'jungleMountains', 'junglePeaks']
+    },
+    'cliffside': {
+        name: 'Cliffside',
+        description: 'Rocky planet with dramatic elevation changes',
+        terrainSet: ['deepCliffOcean', 'shallowCliffOcean', 'cliffBeach', 'cliffPlains', 'cliffHills', 'cliffMountains', 'cliffPeaks']
+    },
+    'desertia': {
+        name: 'Desertia',
+        description: 'Arid planet with vast deserts',
+        terrainSet: ['deepDesertOcean', 'shallowDesertOcean', 'desertBeach', 'desert', 'desertHills', 'desertMountains', 'desertPeaks']
+    },
+    'volcanis': {
+        name: 'Volcanis',
+        description: 'Volcanic planet with lava flows',
+        terrainSet: ['lavaOcean', 'shallowLava', 'lavaBeach', 'volcanicPlains', 'volcanicHills', 'volcanicMountains', 'volcanicPeaks']
+    },
+    'aquaria': {
+        name: 'Aquaria',
+        description: 'Ocean planet with scattered islands',
+        terrainSet: ['deepAquaOcean', 'shallowAquaOcean', 'aquaBeach', 'aquaPlains', 'aquaHills', 'aquaMountains', 'aquaPeaks']
+    },
+    'tundra': {
+        name: 'Tundra',
+        description: 'Cold planet with sparse vegetation',
+        terrainSet: ['deepIce', 'shallowIce', 'iceBeach', 'tundra', 'iceHills', 'iceMountains', 'icePeaks']
+    }
+}; 
